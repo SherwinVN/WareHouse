da1c9b0045195f848adf974ed0cfdb287b0bec2f
VanTrung(VTS) <trungeale@outlook.com>
2020-01-17 17:14:06 +0700

    Add Model

---
+++ Libraries/Develover.Core/Entities/buttonInTag.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace Develover.Core
+{
+    public partial class buttonInTag : DevExpress.XtraEditors.XtraUserControl
+    {
+        string stringDisplay;
+        string stringValue;
+        public buttonInTag(string strDisplay,string strValue)
+        {
+            InitializeComponent();
+            labDisplayValue.Text = stringDisplay = strDisplay;
+            stringValue = strValue;
+            
+            Width = 80;
+            Height = 20;
+        }
+
+        public string StringDisplay { get => StringDisplay; set => StringDisplay = value; }
+        public string StringValue { get => stringValue; set => stringValue = value; }
+
+        private void butDelete_Click(object sender, EventArgs e)
+        {
+            Parent.Controls.Remove(this);
+            this.Dispose();
+        }
+
+
+
+    }
+}
